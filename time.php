790818f8d761274cc632c1a25faee640dc338b93
Rshalika <rshal13@freeuni.edu.ge>
2016-05-26 20:19:57 +0400

    init commit

---
+++ time.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rezo
+ * Date: 5/26/16
+ * Time: 6:02 PM
+ */
+$static_time = ['year' => 2016, 'month'=> 6, 'day'=> 10, 'hour'=> 11, 'ampm'=> 'am'];
+
+
+echo (json_encode($static_time));
